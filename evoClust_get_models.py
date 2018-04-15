af13bcb2d8ad39012ad5bc1f3208df7ef48e4892
magnus <magnus@genesilico.pl>
2018-04-15 18:22:35 +0200

    get_models.py: --native-seq-only mode

--- evoClust_get_models.py
+++ evoClust_get_models.py
@@ -9,6 +9,31 @@ This script creates:
 
 Add cutoff the name of reps, e.g. reps_c2.5
 
+The script has the second mode right now::
+
+    [mm] rosetta-5x$ evoClust_get_models.py -i structures/ ade_plus_ade_cleanup_mapping_pkX_*.out -n adepk
+    evoClust_get_models.py
+    --------------------------------------------------------------------------------
+    ['adepk_min.out.10.pdb', 'adepk_min.out.5.pdb', '', 'adepk_min.out.1.pdb', '']
+    1_adepk_min.out.10.pdb
+    2_adepk_min.out.5.pdb
+    3_
+    4_adepk_min.out.1.pdb
+    5_
+    = structures == out/structures/<files>===================
+    cp -v structures//adepk_min.out.10.pdb reps_ns/c1_adepk_min.out.10.pdb
+    structures//adepk_min.out.10.pdb -> reps_ns/c1_adepk_min.out.10.pdb
+    cp -v structures//adepk_min.out.5.pdb reps_ns/c2_adepk_min.out.5.pdb
+    structures//adepk_min.out.5.pdb -> reps_ns/c2_adepk_min.out.5.pdb
+    cp -v structures// reps_ns/c3_
+    cp: structures// is a directory (not copied).
+    cp -v structures//adepk_min.out.1.pdb reps_ns/c4_adepk_min.out.1.pdb
+    structures//adepk_min.out.1.pdb -> reps_ns/c4_adepk_min.out.1.pdb
+    cp -v structures// reps_ns/c5_
+    cp: structures// is a directory (not copied).
+
+first, the input is parsed to get borders of lines of clusters. These borders are used to select structures that come to a given cluster. For each cluster, there is a search if within it there is a structure that starts with a given name - defined with --NATIVE_SEQ_ONLY. If there is none, then to the reps list '' is appended.
+
 OLD: It reads `out` folder created by evoclustRNA.py in structure such as:
 - out/structures/<homologs>
 """
@@ -28,15 +53,40 @@ def is_number(s):
 
 
 class ClustixResult(object):
-    def __init__(self, fn, input_dir, output_prefix, skip_motifs, use_cutoff_for_names):
+    def __init__(self, fn, input_dir, output_prefix, skip_motifs, use_cutoff_for_names, native_seq_only):
         if output_prefix:
             output_prefix += '_'
 
         self.lines = open(fn).readlines()
-        reps = []
-        for i, l in enumerate(self.lines):
-            if is_number(l):
-                reps.append(self.lines[i + 1].strip())
+
+        # get reps based on structures after numbers, such as
+        # 8.0
+        # adepk_min.out.10.pdb
+        if not native_seq_only:
+            reps = []
+            for i, l in enumerate(self.lines):
+                if is_number(l):
+                    reps.append(self.lines[i + 1].strip())
+
+        # get reps based on structures in a given cluster going from the top of that cluster
+        if native_seq_only:
+            clusters_borders = []
+            for i,l in enumerate(self.lines):
+                if is_number(l):
+                    clusters_borders.append(i)
+
+            clusters_borders.append(len(self.lines))  # at the end of the file
+            reps = []
+            for i in range(0,5): # for top 5 clusters
+                # ['8.0\n', 'adepk_min.out.10.pdb\n', 'a04pk_min.out.2.pdb\n', 'a04pk_min.out.5.pdb\n', 'a04pk_min.out.6.pdb\n', 'a04pk_min.out.7.pdb\n', 'u51pk_min.out.2.pdb\n', 'u51pk_min.out.5.pdb\n', 'u51pk_min.out.10.pdb\n']
+                cluster_lines = self.lines[clusters_borders[i] : clusters_borders[i + 1]]
+                for cl in cluster_lines:
+                    if cl.startswith(native_seq_only):
+                        reps.append(cl.strip())
+                        break
+                else:
+                    reps.append('')  # if not then 'None'
+            print(reps)
 
         for i, r in enumerate(reps):
             print(str(i + 1) + '_' + r)
@@ -47,8 +97,13 @@ class ClustixResult(object):
         else:
             suffix = ''
 
+        # reps
+        if native_seq_only:
+            repsfolder = 'reps_ns'
+        else:
+            repsfolder = 'reps'
         try:
-            os.mkdir(output_prefix + 'reps' + suffix)
+            os.mkdir(output_prefix + repsfolder + suffix)
         except OSError:
             pass
 
@@ -58,24 +113,36 @@ class ClustixResult(object):
         for i, r in enumerate(reps):
             #cmd = "find . -iname " + r
             # commands.getoutput(cmd).split()[0].strip()
-            rpath = 'out/structures/' + r
+
+            if input_dir:
+                rpath = input_dir + os.sep + r
+            else:
+                rpath = 'out/structures/' + r
+
             cmd = ('cp -v ' + rpath + ' ' +
-                   output_prefix + 'reps' + suffix + '/c' + str(i + 1) + '_' + r)
+                   output_prefix + repsfolder + suffix + '/c' + str(i + 1) + '_' + r)
             print cmd
             os.system(cmd)
-        try:
-            os.mkdir(output_prefix + 'reps_motifs' + suffix)
-        except OSError:
-            pass
 
+        # motifs
         if not skip_motifs:
+            if native_seq_only:
+                reps_motifs_folder = 'reps_motifs_ns'
+            else:
+                reps_motifs_folder = 'reps_motifs'
+
+            try:
+                os.mkdir(output_prefix + reps_motifs_folder + suffix)
+            except OSError:
+                pass
+
             print '= motif =========================================='
             for i, r in enumerate(reps):
                 print(input_dir + '/motifs/' + r + ' -> ' +
-                      output_prefix + 'reps_motifs' + suffix + '/' + str(i + 1) + '_' + r)
+                      output_prefix + reps_motifs_folder + suffix + '/' + str(i + 1) + '_' + r)
                 try:
                     shutil.copyfile(input_dir + '/motifs/' + r, output_prefix +
-                                    'reps_motifs' + suffix + '/c' + str(i + 1) + '_' + r)
+                                    reps_motifs_folder + suffix + '/c' + str(i + 1) + '_' + r)
                 except IOError:
                     print 'Missing motifs folder?'
 
@@ -91,6 +158,7 @@ def get_parser():
     parser.add_argument('clustix_results_fn', help="")
     parser.add_argument('-c', '--use-cutoff-for-names', action='store_true')
     parser.add_argument('-s', '--skip_motifs', action='store_true')
+    parser.add_argument('-n', '--native-seq-only')
     return parser
 
 
@@ -100,4 +168,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(os.path.basename(__file__))
     print('-' * 80)
-    ClustixResult(args.clustix_results_fn, args.input_dir, args.output_prefix, args.skip_motifs, args.use_cutoff_for_names)
+    ClustixResult(args.clustix_results_fn, args.input_dir, args.output_prefix, args.skip_motifs, args.use_cutoff_for_names, args.native_seq_only)
